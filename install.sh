ad75c527972ed9299f16a6730e9677e4df469e0c
sumit <sumit@duskbyte.com>
2025-06-27 16:05:59 +0530

    Fixes

--- install.sh
+++ install.sh
@@ -4,22 +4,24 @@ TOKEN=$1
 SERVER_URL=$2
 
 if [ -z "$TOKEN" ] || [ -z "$SERVER_URL" ]; then
-  echo "❌ Error: Token or Server URL missing."
+  echo "Error: Token or Server URL missing."
   echo "Usage: bash -s <AGENT_KEY> <SERVER_URL>"
   exit 1
 fi
+
+
+
+echo "Downloading goAgent.deb package"
+curl -fsSL https://github.com/sumit-duskbyte/go-agent-installer/releases/download/v1.0.0/goAgent.deb -o goAgent.deb
+
+echo "Installing go agent package"
+sudo dpkg -i goAgent.deb
 
 echo "Saving token and server URL to /etc/goAgent/config.yml"
 sudo mkdir -p /etc/goAgent
 
 # Write YAML config
 echo -e "token: \"$TOKEN\"\nserver_url: \"$SERVER_URL\"" | sudo tee /etc/goAgent/config.yml > /dev/null
-
-echo "⬇️ Downloading goAgent.deb package"
-curl -fsSL https://github.com/sumit-duskbyte/go-agent-installer/releases/download/v1.0.0/goAgent.deb -o goAgent.deb
-
-echo "Installing go agent package"
-sudo dpkg -i goAgent.deb
 
 echo "Enabling and starting service"
 sudo systemctl daemon-reload
