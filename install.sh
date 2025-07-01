b6073e7d4843644c1f40e64b65aeb74c8accd06b
sumit <sumit@duskbyte.com>
2025-07-01 17:20:51 +0530

    Fixes

--- install.sh
+++ install.sh
@@ -9,52 +9,6 @@ if [ -z "$TOKEN" ] || [ -z "$SERVER_URL" ]; then
   exit 1
 fi
 
-
-check_ports() {
-  echo "Checking for required ports: 80 (HTTP), 443 (HTTPS), and 5432 (PostgreSQL) ..."
-  local missing_ports=()
-
-  for port in 80 443 5432; do
-    if ss -tuln | awk '{print $5}' | grep -E -q ":$port$|:$port\$"; then
-      echo "✅ Port $port is open."
-    else
-      echo "❌ Port $port is not open."
-      missing_ports+=("$port")
-    fi
-  done
-
-  if [ ${#missing_ports[@]} -gt 0 ]; then
-    echo
-    echo "Required ports not open: ${missing_ports[*]}"
-    echo "Please make sure ports 80 (HTTP), 443 (HTTPS), and 5432 (PostgreSQL) are open before continuing."
-    exit 1
-    
-  fi
-
-  echo
-}
-
-
-check_docker() {
-  echo "Checking if Docker is installed..."
-
-  if command -v docker &> /dev/null; then
-    echo "✅ Docker is installed."
-  else
-    echo "❌ Docker is not installed."
-    echo "Please install Docker before proceeding."
-    exit 1
-  fi
-
-  echo
-}
-
-
-check_ports
-
-check_docker
-
-
 echo "Downloading goAgent.deb package..."
 curl -fsSL https://github.com/sumit-duskbyte/go-agent-installer/releases/download/v1.0.0/goAgent.deb -o goAgent.deb
 
