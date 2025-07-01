cf548c51768e009d85cfea59a2db1555868db14c
sumit <sumit@duskbyte.com>
2025-07-01 15:34:41 +0530

    Fixes

--- install.sh
+++ install.sh
@@ -9,37 +9,49 @@ if [ -z "$TOKEN" ] || [ -z "$SERVER_URL" ]; then
   exit 1
 fi
 
-echo "Checking for Ports 80...."
 
 check_ports() {
+  echo "Checking for Ports 80 and 443...."
   local missing_ports=()
+  local found_ports=()
 
-  for port in 80; do
-    if ! ss -tuln | grep -q ":$port "; then
+  for port in 80 443; do
+    if ss -tuln | grep -q ":$port "; then
+      echo "✅ Port $port is open."
+      found_ports+=("$port")
+    else
+      echo "❌ Port $port is not open."
       missing_ports+=("$port")
     fi
   done
 
   if [ ${#missing_ports[@]} -gt 0 ]; then
     echo "❌ Required ports not open: ${missing_ports[*]}"
-    echo "Please make sure ports 80 are open (listening) before installing."
+    echo "Please make sure ports 80 and 443 are open (listening) before installing."
     exit 1
   fi
+
+  echo
 }
 
-echo "Checking if Docker is installed..."
 
 check_docker() {
-  if ! command -v docker &> /dev/null; then
+  echo "Checking if Docker is installed..."
+
+  if command -v docker &> /dev/null; then
+    echo "✅ Docker is installed."
+  else
     echo "❌ Docker is not installed."
     echo "Please install Docker before proceeding."
     exit 1
   fi
+
+  echo
 }
 
 
 check_ports
-echo "✅ Ports 80 is open."
+echo "✅ Ports 80 and 443 are open."
 
 check_docker
 echo "✅ Docker is installed."
