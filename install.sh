9ebc1e1472d7a2f37e2e36f3e1de3da41f93f772
sumit <sumit@duskbyte.com>
2025-07-01 16:12:16 +0530

    Fixes

--- install.sh
+++ install.sh
@@ -20,7 +20,6 @@ check_ports() {
     else
       echo "❌ Port $port is not open."
       missing_ports+=("$port")
-      exit 1
     fi
   done
 
@@ -28,6 +27,7 @@ check_ports() {
     echo
     echo "Required ports not open: ${missing_ports[*]}"
     echo "Please make sure ports 80 (HTTP), 443 (HTTPS), and 5432 (PostgreSQL) are open before continuing."
+    exit 1
   fi
 
   echo
