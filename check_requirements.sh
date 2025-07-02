e86f8a254cff66d9079941e1dd52f45db6a76d46
sumit <sumit@duskbyte.com>
2025-07-02 16:14:59 +0530

    Fixes

--- check_requirements.sh
+++ check_requirements.sh
@@ -1,3 +1,17 @@
+check_docker() {
+  echo "Checking if Docker is installed..."
+
+  if command -v docker &> /dev/null; then
+    echo "✅ Docker is installed."
+  else
+    echo "❌ Docker is not installed."
+    echo "Please install Docker before proceeding."
+    exit 1
+  fi
+
+  echo
+}
+
 check_ports() {
   echo "Checking for required ports: 80 (HTTP), 443 (HTTPS), and 5432 (PostgreSQL) ..."
   local missing_ports=()
@@ -11,10 +25,8 @@ check_ports() {
       # If port is 80 or 443, check if Docker is using it
       if [[ " ${docker_ports[*]} " =~ " $port " ]]; then
         if ! sudo ss -tulnp | grep -E "LISTEN.+:$port\b" | grep -q "docker"; then
-          echo "❌ Port $port is NOT used by Docker."
+          echo "❌ Port $port is used by other process."
           docker_port_errors+=("$port")
-        else
-          echo "✅ Port $port is correctly used by Docker."
         fi
       fi
 
@@ -33,35 +45,18 @@ check_ports() {
   fi
 
   # If Docker is not using required ports
-  if [ ${#docker_port_errors[@]} -gt 0 ]; then
-    echo
-    echo "❌ Docker is NOT using the following ports: ${docker_port_errors[*]}"
-    echo "Please ensure Docker is running and use these ports."
-    exit 1
-  fi
+  # if [ ${#docker_port_errors[@]} -gt 0 ]; then
+  #   echo
+  #   echo "❌ Docker is NOT using the following ports: ${docker_port_errors[*]}"
+  #   echo "Please ensure Docker is running and use these ports."
+  #   exit 1
+  # fi
 
   echo
-  echo "✅ All required ports are open and correctly used by Docker (80/443) or PostgreSQL (5432)."
-  echo
-}
-
-
-
-check_docker() {
-  echo "Checking if Docker is installed..."
-
-  if command -v docker &> /dev/null; then
-    echo "✅ Docker is installed."
-  else
-    echo "❌ Docker is not installed."
-    echo "Please install Docker before proceeding."
-    exit 1
-  fi
-
+  echo "✅ All required ports are open 80 443 5432."
   echo
 }
-
-
-check_ports
 
 check_docker
+
+check_ports
